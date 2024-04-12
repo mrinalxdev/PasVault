59d1f07368ad11a5942bbddb79227d1b53f091fa
Mrinal Pramanick <mrinalbitsat@gmail.com>
2024-04-13 02:41:13 +0530

    Added Functionality of search and list entries

--- passvault/src/main.rs
+++ passvault/src/main.rs
@@ -1,9 +1,7 @@
 mod psentry;
 
-use std::io::Read;
-
 use crate::psentry::prompt;
-use crate::psentry::read_password_from_file;
+use crate::psentry::read_passwords_from_file;
 
 fn clr() {
     print!("{}[2J", 27 as char);
@@ -36,45 +34,44 @@ fn main() {
     "#;
 
     println!("{ascii}");
+
     loop {
-        println!("Password manager menu: ");
-        println!("1. Add Entry");
-        println!("2. List Entry");
-        println!("3. Search Entry");
+        println!("Password manager menu : ");
+        println!("1. Add Entry ");
+        println!("2. List Entry ");
+        println!("3. Search Entry ");
         println!("4. Quit");
 
         let mut choice = String::new();
         std::io::stdin().read_line(&mut choice).unwrap();
 
         match choice.trim() {
-            "1" => {
-                clr();
-                let entry = ServiceInfo::new()
-            }
+            "1" => {}
             "2" => {
                 clr();
-                let services = read_password_from_file().unwrap_or_else(|err| {
-                    eprint!("Error reading passwords : {}", err);
+                let services = read_passwords_from_file().unwrap_or_else(|err| {
+                    eprintln!("Error reading passwords : {}", err);
                     Vec::new()
                 });
                 for item in &services {
                     println!(
-                        "Service = {}
-                        - Username : {}
-                        - Password : {}",
-                        item.service, item.username, item.password
-                    );
+                        "Services = {}
+                        - Username = {}
+                        - Password = {} ",
+                        item.service, item.usernaem, item.password
+                    )
                 }
             }
             "3" => {
                 clr();
-                let services = read_password_from_file().unwrap_or_else(|err| {
-                    eprint!("Error reading passwords : {}", err);
+                let services = read_passwords_from_file().unwrap_or_else(|err| {
+                    eprintln!("Error reading passwords : {}", err);
                     Vec::new()
                 });
+
                 let search = prompt("Search :");
-                for item in &services {
-                    if item.service.as_str() == search.as_str() {
+                for item  in &services {
+                    if item.service.as_str() == search.as_str(){
                         println!(
                             "Service = {}
                             - Username : {}
@@ -86,10 +83,10 @@ fn main() {
             }
             "4" => {
                 clr();
-                println!("Goodbye !");
+                println!("Have a great day!");
                 break;
             }
-            _ => println!("Invalid Choice. ")
+            _ => println!("Invalid choice. ")
         }
         println!("\n\n");
     }
